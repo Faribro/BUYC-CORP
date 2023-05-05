a7f92ad462caf0c6e7f60165c07e64f64af24568
Farid Sayyed <49672595+Faribro@users.noreply.github.com>
2023-05-05 13:02:33 +0000

    Update App.tsx and index.tsx

--- buyc/src/App.tsx
+++ buyc/src/App.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 import "./App.css";
+import { HomePage } from "./app/containers/HomePage";
+import tw from "twin.macro";
+import styled from "styled-components";
 
+const AppContainer = styled.div`
+  ${tw`
+w-full
+h-full
+flex
+flex-col
+`}
+`;
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
+  );
 }
 
 export default App;
